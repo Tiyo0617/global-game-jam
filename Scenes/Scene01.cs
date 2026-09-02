340eb3d1540aeae1afc7869e3e43495adae8aa6d
suchuanwu1126 <1514738265@qq.com>
2026-09-02 19:47:00 +0800

    wyx

--- Scenes/Scene01.cs
+++ Scenes/Scene01.cs
@@ -12,5 +12,6 @@ public partial class Scene01 : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		sgsk
 	}
 }
